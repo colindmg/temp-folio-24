6132799e4f685f7b283b13bd8e2e4b603f6e09ff
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-06-09 21:41:26 -0400

    Anim des liens et retrait du mouse move effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import noiseImage from "../public/img/noise.webp";
-// import BubbleGradient from "./components/BubbleGradient";
+import BubbleGradient from "./components/BubbleGradient";
 
 function App() {
   return (
     <>
-      <div className="relative text-white h-screen w-screen bg-[#141414] flex flex-col justify-between p-24">
+      <div className="relative text-white h-screen w-screen bg-[#141414] flex flex-col justify-between items-start lg:p-24 md:p-20 sm:p-16 p-12">
         <div className="z-50">
           <h1 className="text-4xl relative font-grotesk">Col&apos;</h1>
           <h2 className="text-2xl font-grotesk font-thin mt-1">
             Freelance web developer
           </h2>
         </div>
-        <div className="z-50 flex flex-col gap-1 font-grotesk text-xl">
+        <div className="z-50 flex flex-col gap-1 font-grotesk text-xl items-start">
           <a href="https://x.com/colindmg" className="group">
             →{" "}
             <span className="group-hover:underline underline-offset-4 decoration-[1.5px]">
@@ -35,7 +35,7 @@ function App() {
           </a>
         </div>
 
-        {/* <BubbleGradient /> */}
+        <BubbleGradient />
         <div
           className="absolute top-0 left-0 w-full h-full z-30 bg-repeat opacity-[0.03]"
           style={{ backgroundImage: `url(${noiseImage})` }}
--- src/components/BubbleGradient.jsx
+++ src/components/BubbleGradient.jsx
@@ -1,37 +1,33 @@
 import { useEffect, useRef } from "react";
 
 const BubbleGradient = () => {
-  const interBubbleRef = useRef(null);
+  // const interBubbleRef = useRef(null);
 
   useEffect(() => {
-    const interBubble = interBubbleRef.current;
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
-
-    function move() {
-      curX += (tgX - curX) / 20;
-      curY += (tgY - curY) / 20;
-      if (interBubble) {
-        interBubble.style.transform = `translate(${Math.round(
-          curX
-        )}px, ${Math.round(curY)}px)`;
-      }
-      requestAnimationFrame(move);
-    }
-
-    function handleMouseMove(event) {
-      tgX = event.clientX;
-      tgY = event.clientY;
-    }
-
-    window.addEventListener("mousemove", handleMouseMove);
-    move();
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
+    // const interBubble = interBubbleRef.current;
+    // let curX = 0;
+    // let curY = 0;
+    // let tgX = 0;
+    // let tgY = 0;
+    // function move() {
+    //   curX += (tgX - curX) / 20;
+    //   curY += (tgY - curY) / 20;
+    //   if (interBubble) {
+    //     interBubble.style.transform = `translate(${Math.round(
+    //       curX
+    //     )}px, ${Math.round(curY)}px)`;
+    //   }
+    //   requestAnimationFrame(move);
+    // }
+    // function handleMouseMove(event) {
+    //   tgX = event.clientX;
+    //   tgY = event.clientY;
+    // }
+    // window.addEventListener("mousemove", handleMouseMove);
+    // move();
+    // return () => {
+    //   window.removeEventListener("mousemove", handleMouseMove);
+    // };
   }, []);
 
   return (
@@ -60,7 +56,7 @@ const BubbleGradient = () => {
         <div className="g3"></div>
         <div className="g4"></div>
         <div className="g5"></div>
-        <div className="interactive" ref={interBubbleRef}></div>
+        {/* <div className="interactive" ref={interBubbleRef}></div> */}
       </div>
     </div>
   );
