fb1e022ad61602e4b4a8f5734b74b32ed23bfb47
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-06-09 21:35:32 -0400

    Ajout des liens

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,37 @@ import noiseImage from "../public/img/noise.webp";
 function App() {
   return (
     <>
-      <div className="relative text-white h-screen w-screen bg-[#141414] flex flex-col p-24">
-        <h1 className="text-4xl relative z-50 font-grotesk">colindmg</h1>
-        <h2 className="text-2xl font-grotesk font-thin">
-          Freelance web developer
-        </h2>
+      <div className="relative text-white h-screen w-screen bg-[#141414] flex flex-col justify-between p-24">
+        <div className="z-50">
+          <h1 className="text-4xl relative font-grotesk">Col&apos;</h1>
+          <h2 className="text-2xl font-grotesk font-thin mt-1">
+            Freelance web developer
+          </h2>
+        </div>
+        <div className="z-50 flex flex-col gap-1 font-grotesk text-xl">
+          <a href="https://x.com/colindmg" className="group">
+            →{" "}
+            <span className="group-hover:underline underline-offset-4 decoration-[1.5px]">
+              TWITTER / X
+            </span>
+          </a>
+          <a href="https://github.com/colindmg" className="group">
+            →{" "}
+            <span className="group-hover:underline underline-offset-4 decoration-[1.5px]">
+              GITHUB
+            </span>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/colin-demouge/"
+            className="group"
+          >
+            →{" "}
+            <span className="group-hover:underline underline-offset-4 decoration-[1.5px]">
+              LINKEDIN
+            </span>
+          </a>
+        </div>
+
         {/* <BubbleGradient /> */}
         <div
           className="absolute top-0 left-0 w-full h-full z-30 bg-repeat opacity-[0.03]"
