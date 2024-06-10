649b19bbd8b83ae7749a06f6a798367145d72b37
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-06-09 21:26:01 -0400

    Ajout de la font grotesk

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import noiseImage from "../public/img/noise.webp";
-import BubbleGradient from "./components/BubbleGradient";
+// import BubbleGradient from "./components/BubbleGradient";
 
 function App() {
   return (
     <>
-      <div className="relative h-screen w-screen">
-        <h1 className="text-4xl font-bold uppercase relative z-50">COLINDMG</h1>
-        <BubbleGradient />
+      <div className="relative text-white h-screen w-screen bg-[#141414] flex flex-col p-24">
+        <h1 className="text-4xl relative z-50 font-grotesk">colindmg</h1>
+        <h2 className="text-2xl font-grotesk font-thin">
+          Freelance web developer
+        </h2>
+        {/* <BubbleGradient /> */}
         <div
           className="absolute top-0 left-0 w-full h-full z-30 bg-repeat opacity-[0.03]"
           style={{ backgroundImage: `url(${noiseImage})` }}
