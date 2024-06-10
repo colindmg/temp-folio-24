db16955e2a30562a8a5b72b94f07f84415986802
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-06-09 21:16:41 -0400

    Ajout de grain au gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import noiseImage from "../public/img/noise.webp";
 import BubbleGradient from "./components/BubbleGradient";
 
 function App() {
   return (
     <>
       <div className="relative h-screen w-screen">
-        <h1 className="text-4xl font-bold uppercase relative z-20">COLINDMG</h1>
+        <h1 className="text-4xl font-bold uppercase relative z-50">COLINDMG</h1>
         <BubbleGradient />
+        <div
+          className="absolute top-0 left-0 w-full h-full z-30 bg-repeat opacity-[0.03]"
+          style={{ backgroundImage: `url(${noiseImage})` }}
+        ></div>
       </div>
     </>
   );
