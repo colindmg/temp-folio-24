77ea2edf76cda9e2d7b22116aedc9a4f8db02988
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-06-09 21:06:29 -0400

    Composant perso pour le gradient de fond

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,11 @@
-import { useEffect, useRef } from "react";
+import BubbleGradient from "./components/BubbleGradient";
 
 function App() {
-  const interBubbleRef = useRef(null);
-
-  useEffect(() => {
-    const interBubble = interBubbleRef.current;
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
-
-    function move() {
-      curX += (tgX - curX) / 20;
-      curY += (tgY - curY) / 20;
-      if (interBubble) {
-        interBubble.style.transform = `translate(${Math.round(
-          curX
-        )}px, ${Math.round(curY)}px)`;
-      }
-      requestAnimationFrame(move);
-    }
-
-    function handleMouseMove(event) {
-      tgX = event.clientX;
-      tgY = event.clientY;
-    }
-
-    window.addEventListener("mousemove", handleMouseMove);
-    move();
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
-
   return (
     <>
       <div className="relative h-screen w-screen">
         <h1 className="text-4xl font-bold uppercase relative z-20">COLINDMG</h1>
-        <div className="gradient-bg absolute top-0 left-0 h-full w-full z-10">
-          <svg xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <filter id="goo">
-                <feGaussianBlur
-                  in="SourceGraphic"
-                  stdDeviation="10"
-                  result="blur"
-                />
-                <feColorMatrix
-                  in="blur"
-                  mode="matrix"
-                  values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-                  result="goo"
-                />
-                <feBlend in="SourceGraphic" in2="goo" />
-              </filter>
-            </defs>
-          </svg>
-          <div className="gradients-container">
-            <div className="g1"></div>
-            <div className="g2"></div>
-            <div className="g3"></div>
-            <div className="g4"></div>
-            <div className="g5"></div>
-            <div className="interactive" ref={interBubbleRef}></div>
-          </div>
-        </div>
+        <BubbleGradient />
       </div>
     </>
   );
