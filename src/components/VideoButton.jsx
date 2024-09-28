a004a6d0f0e2ae48a211d9b4814048a64748a1a7
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-28 21:11:37 +0200

    Fix de conflit entre animation css et framer motion sur le bouton vidéo

--- src/components/VideoButton.jsx
+++ src/components/VideoButton.jsx
@@ -1,36 +1,53 @@
-import { easeInOut, motion } from "framer-motion";
-import { useState } from "react";
-
+import clsx from "clsx";
+import { easeInOut, motion, useIsPresent } from "framer-motion";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 const VideoButton = ({ playCloseSound, playOpenSound }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isOpenButtonShown, setIsOpenButtonShown] = useState(true);
+  const isPresent = useIsPresent();
+  const [addTransition, setAddTransition] = useState(false);
+
+  useEffect(() => {
+    if (isPresent) {
+      setTimeout(() => {
+        setAddTransition(true);
+      }, 1750);
+    }
+  }, [isPresent]);
 
   return (
     <motion.div
+      key={"video-button"}
       initial={{ opacity: 0, filter: "blur(5px)" }}
       animate={{
         opacity: 1,
         filter: "blur(0px)",
         transition: { duration: 0.75, delay: 1.0, ease: easeInOut },
       }}
-      className={`flex gap-2 items-start mt-1 ${
-        isOpen ? "" : "-translate-x-2"
-      } transition-all duration-500`}
+      className={clsx(
+        "flex gap-2 items-start mt-1",
+        isOpen ? "" : "-translate-x-2",
+        addTransition && "transition-all duration-500"
+      )}
     >
       {/* DIV CONTENANT LA VIDÉO */}
       <div
-        className={`rounded-lg bg-white ${
-          isOpen ? "p-1 w-[508px]" : "w-0"
-        } transition-all duration-500`}
+        className={clsx(
+          "rounded-lg bg-white",
+          isOpen ? "p-1 w-[508px]" : "w-0",
+          addTransition && "transition-all duration-500"
+        )}
         style={{ maxWidth: isOpen ? "calc(68vw + 8px)" : "0" }}
       >
         <video
           src="/videos/compil2.mp4"
-          className={` ${
-            isOpen ? "w-[500px] max-w-[68vw] opacity-100" : "w-0 opacity-0"
-          } transition-all duration-500 rounded-md`}
+          className={clsx(
+            isOpen ? "w-[500px] max-w-[68vw] opacity-100" : "w-0 opacity-0",
+            addTransition && "transition-all duration-500",
+            "rounded-md"
+          )}
           autoPlay
           muted
           loop
@@ -40,22 +57,27 @@ const VideoButton = ({ playCloseSound, playOpenSound }) => {
 
       {/* BOUTON D'OUVERTURE DE LA VIDÉO */}
       <button
-        className={`group flex relative uppercase tracking-wider w-fit text-neutral-800 bg-white px-4 py-0.5 cursor-pointer pointer-events-auto rounded-full transition-all duration-500 overflow-hidden ${
-          !isOpenButtonShown ? "opacity-0" : ""
-        } ${isOpen ? "hidden" : ""}`}
+        className={clsx(
+          "group flex relative uppercase tracking-wider w-fit text-neutral-800 bg-white px-4 py-0.5 cursor-pointer pointer-events-auto rounded-full",
+          addTransition && "transition-all duration-500",
+          "overflow-hidden",
+          !isOpenButtonShown && "opacity-0",
+          isOpen && "hidden"
+        )}
         onClick={() => {
           setIsOpen(!isOpen);
           playOpenSound();
           setIsOpenButtonShown(false);
         }}
       >
-        <p className={`${isOpen ? "opacity-0 w-0" : "opacity-100"} mr-2`}>
+        <p className={clsx(isOpen ? "opacity-0 w-0" : "opacity-100", "mr-2")}>
           WORK EXCERPT
         </p>
         <p
-          className={`${
-            isOpen ? "opacity-0 w-0" : "opacity-100"
-          } group-hover:animate-spin`}
+          className={clsx(
+            isOpen ? "opacity-0 w-0" : "opacity-100",
+            "group-hover:animate-spin"
+          )}
         >
           ✶
         </p>
@@ -63,9 +85,11 @@ const VideoButton = ({ playCloseSound, playOpenSound }) => {
 
       {/* BOUTON DE FERMETURE DE LA VIDÉO */}
       <button
-        className={`group flex relative items-center uppercase tracking-wider text-neutral-800 bg-white cursor-pointer pointer-events-auto rounded-full transition-all duration-500 delay-300 ${
+        className={clsx(
+          "group flex relative items-center uppercase tracking-wider text-neutral-800 bg-white cursor-pointer pointer-events-auto rounded-full",
+          addTransition && "transition-all duration-500 delay-300",
           isOpen ? "w-8 h-8 opacity-100" : "opacity-0"
-        }`}
+        )}
         onClick={() => {
           setIsOpen(!isOpen);
           playCloseSound();
@@ -73,9 +97,10 @@ const VideoButton = ({ playCloseSound, playOpenSound }) => {
         }}
       >
         <p
-          className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:rotate-45 transition-all duration-300 ${
-            isOpen ? "" : "hidden"
-          }`}
+          className={clsx(
+            "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:rotate-45 transition-all duration-300",
+            !isOpen && "hidden"
+          )}
         >
           ✦
         </p>
